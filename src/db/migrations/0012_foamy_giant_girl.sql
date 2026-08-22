156cc528734413ef3dcf99ed4def48679a007cec
Omar Salama <omar550205@gmail.com>
2026-08-22 11:23:08 +0300

    migration: added temp translation objects to these tables (chefs, users, admins)

---
+++ src/db/migrations/0012_foamy_giant_girl.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "users" ADD COLUMN "first_name_obj" jsonb DEFAULT '{"en":"","ar":""}'::jsonb NOT NULL;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "last_name_obj" jsonb DEFAULT '{"en":"","ar":""}'::jsonb NOT NULL;--> statement-breakpoint
+ALTER TABLE "admins" ADD COLUMN "name_obj" jsonb DEFAULT '{"en":"","ar":""}'::jsonb NOT NULL;--> statement-breakpoint
+ALTER TABLE "chefs" ADD COLUMN "name_obj" jsonb DEFAULT '{"en":"","ar":""}'::jsonb NOT NULL;
